3ae75984918f74cd8a733fc855cb4dbdce4f3280
HugeKyung <kiki9510@gmail.com>
2025-05-17 18:39:24 +0900

    feat: 각 서버별 Dockerfile 추가, eventModule에 DB 연결을 위한 코드 추가

--- apps/event/src/event.module.ts
+++ apps/event/src/event.module.ts
@@ -1,10 +1,33 @@
+import { getMongoUri } from '@libs/database/config/db.config';
+import { EventSchema } from '@libs/database/schemas/event.schema';
+import {
+    RewardHistory,
+    RewardHistorySchema,
+} from '@libs/database/schemas/reward-history.schema';
+import { Reward, RewardSchema } from '@libs/database/schemas/reward.schema';
 import { Module } from '@nestjs/common';
-import { EventController } from './event.controller';
-import { EventService } from './event.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [EventController],
-  providers: [EventService],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (config: ConfigService) => ({
+                uri: getMongoUri({
+                    host: config.get<string>('MONGO_HOST')!,
+                    port: parseInt(config.get<string>('MONGO_PORT') ?? '27017'),
+                    dbName: config.get<string>('MONGO_DB_NAME')!,
+                }),
+            }),
+            inject: [ConfigService],
+        }),
+        MongooseModule.forFeature([
+            { name: Event.name, schema: EventSchema },
+            { name: Reward.name, schema: RewardSchema },
+            { name: RewardHistory.name, schema: RewardHistorySchema },
+        ]),
+    ],
 })
-export class EventModule {}
+export class AppModule {}
