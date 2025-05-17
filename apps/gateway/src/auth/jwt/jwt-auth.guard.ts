3749330d79a540d2055d11303eff18432d04c7b2
HugeKyung <kiki9510@gmail.com>
2025-05-18 03:07:45 +0900

    feat: guard에서 제외할 request.path 조건 추가

--- apps/gateway/src/auth/jwt/jwt-auth.guard.ts
+++ apps/gateway/src/auth/jwt/jwt-auth.guard.ts
@@ -1,5 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') implements CanActivate {
+    canActivate(context: ExecutionContext): boolean {
+        const request: Request = context.switchToHttp().getRequest();
+        const path = request.path;
+        if (path === '/api/auth/login' || path === '/api/auth/register') {
+            return true;
+        }
+
+        return super.canActivate(context) as boolean;
+    }
+}
--- apps/gateway/src/auth/role/role.guard.ts
+++ apps/gateway/src/auth/role/role.guard.ts
@@ -1,5 +1,6 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { Request } from 'express';
 import { UserRole } from '../../common/enum/user-role.enum';
 import { JwtPayload } from '../../common/interface/jwt-payload.interface';
 import { ROLES_KEY } from './role.decorator';
@@ -9,6 +10,12 @@ export class RolesGuard implements CanActivate {
     constructor(private reflector: Reflector) {}
 
     canActivate(context: ExecutionContext): boolean {
+        const request: Request = context.switchToHttp().getRequest();
+        const path = request.path;
+        if (path === '/api/auth/login' || path === '/api/auth/register') {
+            return true;
+        }
+
         const canRequestRoles = this.reflector.getAllAndOverride<UserRole[]>(
             ROLES_KEY,
             [context.getHandler(), context.getClass()],
